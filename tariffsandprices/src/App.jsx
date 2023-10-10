92f9c949de61a47070029904b2890c8188c7bc4a
AnnaStaySia <siadan1504@gmail.com>
2023-10-10 21:05:15 +0300

    second commit

--- tariffsandprices/src/App.jsx
+++ tariffsandprices/src/App.jsx
@@ -1,3 +1,5 @@
+import React from 'react'
+import {useState} from 'react'
 import './style/App.scss';
 import Tarif from './Tarif'
 
@@ -7,35 +9,58 @@ const arr = [
         price: 200 +'руб/мес',
         speed: 'до 10 Мбит/сек',
         info: 'Объем включенного трафика не ограничен',
-        color: 'skyblue'
+        color: 'skyblue',
+        id: 1
     },
     {
         title: 'Безлимитный 450',
         price: 400 +'руб/мес',
         speed: 'до 50 Мбит/сек',
         info: 'Объем включенного трафика не ограничен',
-        color: 'lightgreen'
+        color: 'lightgreen',
+        id: 2
     },
     {
         title: 'Безлимитный 550',
         price: 600 +'руб/мес',
         speed: 'до 100 Мбит/сек',
         info: 'Объем включенного трафика не ограничен',
-       color: 'red' 
+       color: 'red',
+       id: 3 
     },
     {
         title: 'Безлимитный 1000',
         price: 800 +'руб/мес',
         speed: 'до 200 Мбит/сек',
         info: 'Объем включенного трафика не ограничен',
-        color: 'gray'
+        color: 'gray',
+        id: 4  
     }
 ]
 
 export default function App(){
+const [id, setID] = useState(0)
+
     return(
+        <>
             <div className='wrapper'>
-                {arr.map(item=><Tarif{...item}/>)}
+                {arr.map((item, index)=> <Tarif
+                item={item}
+                key={index}
+                activeTarif={item.id === id}
+                setID={setID}
+                />)}
             </div>
-    );
+        </>
+    )
 }
+
+
+    
+    // return(
+    //     <>
+    //         <div className='wrapper'>
+    //             {arr.map(item=><Tarif{...item}/>)}
+    //         </div>
+    //     </>
+    // );
--- tariffsandprices/src/Tarif.jsx
+++ tariffsandprices/src/Tarif.jsx
@@ -1,22 +1,31 @@
 import style from './style/tarif.module.scss'
+import React from 'react'
 
-export default function Tarif({title, price, speed, info, color}){
+export default function Tarif({item, activeTarif, setID}){
+    const cl = activeTarif ? '.activeTarif' : null
+
+    function handleActive(){
+        setID(item.id)
+    }
+
     return(
-        <div className={style[color]}>
-            <div className='titleWrapper'>
-                <p className='title'>{title}</p>
-            </div>   
+        <div className={style[item.color]}>
+            <div className={`activeTarif {${cl}`} onClick={handleActive}>
+                <div className='titleWrapper'>
+                    <p className='title'>{item.title}</p>
+                </div>   
 
-            <div className='priceWrapper'>
-                <p className='price'>{price}</p>
-            </div>
+                <div className='priceWrapper'>
+                    <p className='price'>{item.price}</p>
+                </div>
 
-            <div className='speedWrapper'>
-                <p className='speed'>{speed}</p>
-            </div>
+                <div className='speedWrapper'>
+                    <p className='speed'>{item.speed}</p>
+                </div>
 
-            <div className='infoWrapper'>
-                <p className='info'>{info}</p>
+                <div className='infoWrapper'>
+                    <p className='info'>{item.info}</p>
+                </div>
             </div>
         </div>   
     );
