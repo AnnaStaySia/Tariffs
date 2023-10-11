f9706fe935cbbd858ba21866bb859e12d236151c
AnnaStaySia <siadan1504@gmail.com>
2023-10-11 21:40:00 +0300

    third commit

--- tariffsandprices/src/Tarif.jsx
+++ tariffsandprices/src/Tarif.jsx
@@ -1,32 +1,31 @@
-import style from './style/tarif.module.scss'
 import React from 'react'
+import style from './style/tarif.module.scss'
 
 export default function Tarif({item, activeTarif, setID}){
-    const cl = activeTarif ? '.activeTarif' : null
+    const colorStyle=style[item.color]
+    const activeStyle = activeTarif ? style.activeTarif : '';
 
     function handleActive(){
         setID(item.id)
     }
 
-    return(
-        <div className={style[item.color]}>
-            <div className={`activeTarif {${cl}`} onClick={handleActive}>
-                <div className='titleWrapper'>
-                    <p className='title'>{item.title}</p>
-                </div>   
-
-                <div className='priceWrapper'>
-                    <p className='price'>{item.price}</p>
-                </div>
-
-                <div className='speedWrapper'>
-                    <p className='speed'>{item.speed}</p>
-                </div>
-
-                <div className='infoWrapper'>
-                    <p className='info'>{item.info}</p>
-                </div>
-            </div>
+return(
+    <div className={`${colorStyle} ${activeStyle}`} onClick={handleActive}>
+        <div className='titleWrapper'>
+            <p className='title'>{item.title}</p>
         </div>   
-    );
+
+        <div className='priceWrapper'>
+            <p className='price'>{item.price}</p>
+        </div>
+
+        <div className='speedWrapper'>
+            <p className='speed'>{item.speed}</p>
+        </div>
+
+        <div className='infoWrapper'>
+            <p className='info'>{item.info}</p>
+        </div>
+    </div>
+);
 }
